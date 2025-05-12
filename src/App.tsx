dca85fd6d125e9610af5103f8a823898ac65488a
Thien <tranngocthien628@gmail.com>
2025-05-12 23:52:16 +0700

    chore(shadcn): add and test shadcn button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-function App() {
+import { useState } from "react"
+import { Button } from "./components/ui/button";
 
+function App() {
+  const [count, setCount] = useState<number>(0);
   return (
-    <div className="font-bold">
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
       Hello world
+      <Button 
+        variant='outline'
+        onClick={() => setCount(count + 1)}
+      >
+        {count}
+      </Button>
     </div>
   )
 }
