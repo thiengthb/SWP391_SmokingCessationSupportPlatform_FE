b79772a4c9c0d69b2d01f39b2b383fa167946241
Thien <tranngocthien628@gmail.com>
2025-05-13 00:00:42 +0700

    feat(app): implement shadcn dark mode - theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { useState } from "react"
-import { Button } from "./components/ui/button";
+import { ModeToggle } from "./components/theme/theme-toggle";
 
 function App() {
-  const [count, setCount] = useState<number>(0);
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
       Hello world
-      <Button 
-        variant='outline'
-        onClick={() => setCount(count + 1)}
-      >
-        {count}
-      </Button>
+      <ModeToggle />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from './components/theme/theme-provider.tsx'
 import router from './router/index.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
