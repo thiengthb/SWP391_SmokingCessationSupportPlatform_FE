dd369157500c42676ed4acf39c0c8d1a90fa2f17
thi3n <136480142+thiengthb@users.noreply.github.com>
2025-05-31 12:42:28 +0700

    fix: reafactor auth pages, fixing responsive ui

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <div className="h-[calc(100vh-30rem)] w-full flex flex-col items-center justify-center gap-6">
+    <div className="h-[calc(100vh-10rem)] 2xl:h-[calc(100vh-30rem)] w-full flex flex-col items-center px-2 sm:px-0 justify-center gap-6">
       <div className="space-y-4 text-center">
         <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
           404
@@ -12,7 +12,8 @@ const NotFoundPage = () => {
           Page Not Found
         </h2>
         <p className="text-muted-foreground max-w-[42rem] leading-normal sm:text-xl sm:leading-8">
-          Sorry, we couldn't find the page you're looking for. The page might have been removed or the link might be broken.
+          Sorry, we couldn't find the page you're looking for. The page might
+          have been removed or the link might be broken.
         </p>
       </div>
       <Button asChild>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
 import SocialLoginPage from "../pages/SocialLoginPage";
 import SignupPage from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
@@ -8,36 +7,41 @@ import InsurersPage from "../pages/InsurersPage";
 import ForCompaniesPage from "../pages/ForCompaniesPage";
 import PartnershipPage from "../pages/PartnershipPage";
 import DashboardPage from "../pages/DashboardPage";
-import Test from "@/pages/test";
-import LoginPage from "@/pages/auth";
+
+import App from "../App";
 import Layout from "@/components/layout";
-import ForgotPasswordPage from "@/pages/auth/forgotPassPage";
 import NotFoundPage from "@/pages/not-found";
+import Test from "@/pages/test";
+import LoginPage from "@/pages/auth/login";
+import RegisterPage from "@/pages/auth/register";
+import ForgotPasswordPage from "@/pages/auth/forgot-password";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Layout />,
     children: [
-      { path: '/', element: <App /> },
-      { path: '/auth', element: <LoginPage />, children: [
-            { path: 'login'},
-            { path: 'register'},
-        ]
+      { path: "/", element: <App /> },
+      {
+        path: "/auth",
+        children: [
+          { path: "login", element: <LoginPage /> },
+          { path: "register", element: <RegisterPage /> },
+          { path: "forgot-password", element: <ForgotPasswordPage /> },
+        ],
       },
-      { path: '/auth/forgot-password', element: <ForgotPasswordPage /> }, // Redirect to login page
-      { path: '/social-login', element: <SocialLoginPage /> },
-      { path: '/signup', element: <SignupPage /> },
-      { path: '/signin', element: <SignIn /> },
-      { path: '/forgot-password', element: <ForgotPassword /> },
-      { path: '/for-insurers', element: <InsurersPage /> },
-      { path: '/for-companies', element: <ForCompaniesPage /> },
-      { path: '/partnerships', element: <PartnershipPage /> },
-      { path: '/dashboard', element: <DashboardPage /> },
-      { path: '/test', element: <Test /> },
-      { path: '*', element: <NotFoundPage/> }
-    ]
-  }
+      { path: "/social-login", element: <SocialLoginPage /> },
+      { path: "/signup", element: <SignupPage /> },
+      { path: "/signin", element: <SignIn /> },
+      { path: "/forgot-password", element: <ForgotPassword /> },
+      { path: "/for-insurers", element: <InsurersPage /> },
+      { path: "/for-companies", element: <ForCompaniesPage /> },
+      { path: "/partnerships", element: <PartnershipPage /> },
+      { path: "/dashboard", element: <DashboardPage /> },
+      { path: "/test", element: <Test /> },
+      { path: "*", element: <NotFoundPage /> },
+    ],
+  },
 ]);
 
 export default router;
