58e3caf2f73186945049aa4985a968513585a0ea
Hoang Tung Lam <lamhoang24200@gmail.com>
2025-07-05 21:58:34 +0700

    feat: create bookings at memberdashboard

--- src/pages/dashboard/member/index.tsx
+++ src/pages/dashboard/member/index.tsx
@@ -2,24 +2,34 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { OverviewTab } from "./components/OverviewTab";
 import { AchievementsTab } from "./components/AchievementsTab";
 import GoalManagement from "./users";
-
+import { BookingsTab } from "./components/BookingTab";
 export default function MemberDashboard() {
   return (
     <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Member Dashboard</h2>
       </div>
-      
+
       <Tabs defaultValue="overview" className="space-y-4">
         <TabsList>
           <TabsTrigger value="overview">Overview</TabsTrigger>
           <TabsTrigger value="goals">Goals</TabsTrigger>
           <TabsTrigger value="achievements">Achievements</TabsTrigger>
+          <TabsTrigger value="bookings">Bookings</TabsTrigger>
         </TabsList>
-        
-        <TabsContent value="overview"><OverviewTab /></TabsContent>
-        <TabsContent value="goals"><GoalManagement /></TabsContent>
-        <TabsContent value="achievements"><AchievementsTab /></TabsContent>
+
+        <TabsContent value="overview">
+          <OverviewTab />
+        </TabsContent>
+        <TabsContent value="goals">
+          <GoalManagement />
+        </TabsContent>
+        <TabsContent value="achievements">
+          <AchievementsTab />
+        </TabsContent>
+        <TabsContent value="bookings">
+          <BookingsTab />
+        </TabsContent>
       </Tabs>
     </div>
   );
